afc819399cb45733f69c8c169e010624f4d70721
Andrey Dubas <andreydubas1991@gmail.com>
2014-07-08 01:41:26 +0300

    trivial tests are passed

--- diploma_test/main.cpp
+++ diploma_test/main.cpp
@@ -1,17 +1,5 @@
-//#include "../diploma/vector.h"
-//#include "../diploma/matrix.h"
-//#include "../diploma/SquareMatrix.h"
-#include "matrix_test.h"
-#include <iostream>
 #include <gtest.h>
-
-
-
-TEST(FactorialTest, HandlesPositiveInput) {
-//	Util::Matrix<int, 1, 1> m;
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(2, 1);
-}
+#include "matrix_test.h"
 
 int main(int argc, char **argv) {
 	  ::testing::InitGoogleTest(&argc, argv);
---
+++ diploma_test/matrix_test1.h
@@ -0,0 +1,22 @@
+#ifndef MATRIX_TEST_H
+#define MATRIX_TEST_H
+
+#include <gtest/gtest.h>
+#include "../diploma/matrix.h"
+#include "../diploma/SquareMatrix.h"
+
+class CTestMatrix: public ::testing::Test 
+{
+    void determinant_2_validData_ok();
+};
+
+
+using namespace Util;
+
+TEST_F(CTestMatrix, determinant_2_valid_data_ok)
+{
+Matrix<float, 2, 2> m = {1, 2, 
+						3, 4};
+}
+
+#endif // MATRIX_TEST_H
